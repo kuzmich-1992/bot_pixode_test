e805895438f2641d3f9e28afacf9ad3f0da7175b
Dima <kuzmichman92@gmail.com>
2020-11-18 20:40:19 +0200

    bot send message now

--- app/controllers/athlets_controller.rb
+++ app/controllers/athlets_controller.rb
@@ -1,19 +1,16 @@
 class AthletsController < ActionController::Base
 require 'rest-client'
 require 'telegram/bot'
-TOKEN = '1288229656:AAHh9REQgomtG7Q1YFfK3AeTgn2lvxsr71g'
 ANSWER = Athlet.maximum(:result)
-  
+TELEGRAM_BOT_TOKEN = '1288229656:AAHh9REQgomtG7Q1YFfK3AeTgn2lvxsr71g'
+TELEGRAM_CHAT_ID = '-440848523' 
   def index
     @athlets = Athlet.all
   end
   
   def send_message
-    Telegram::Bot::Client.run(TOKEN) do |bot|
-      bot.listen do |message|
-        bot.api.send_message(chat_id: message.chat.id, text: "best result is - #{ANSWER}")
-      end
+    Telegram::Bot::Client.run(TELEGRAM_BOT_TOKEN) do |bot|
+      bot.api.send_message(chat_id: "#{TELEGRAM_CHAT_ID}", text: "best result is - #{ANSWER}")
     end
   end
-  
 end
